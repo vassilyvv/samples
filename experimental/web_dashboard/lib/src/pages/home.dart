288f3ce219bb0facdf231a84840afc55ce014aa6
Arsenii Lisunov <arsenii.lis@gmail.com>
2020-10-16 19:02:47 +0600

    Fix adaptive behavior for appbar icons and notifications

--- experimental/web_dashboard/lib/src/pages/home.dart
+++ experimental/web_dashboard/lib/src/pages/home.dart
@@ -57,42 +57,47 @@ class _HomePageState extends State<HomePage> {
       actions: [
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: FlatButton(
-            onPressed: () => _handleSignOut(),
-            child: Container(
-              margin: EdgeInsets.all(16.0),
-              width: 32,
-              height: 32,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage("assets/avatar.png"),
-                ),
+          child: Container(
+            // margin: EdgeInsets.all(16.0),
+            width: 32,
+            height: 32,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage("assets/avatar.png"),
               ),
             ),
           ),
         ),
-        Stack(children: <Widget>[
-          Icon(Icons.notifications),
-          Positioned(
-            // draw a red marble
-            top: 0.0,
-            right: 0.0,
-            child: Container(
-              width: 16.0,
-              height: 16.0,
-              child: Align(
-                child: Text(
-                  NOTIFICATION_COUNT.toString(),
-                  style: TextStyle(color: Colors.white, fontSize: 11, fontWeight: FontWeight.bold),
+        Container(
+          width: 32,
+          height: 32,
+          margin: const EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          child: Stack(
+            children: <Widget>[
+              Icon(Icons.notifications),
+              Positioned(
+                // draw a red marble
+                top: 0.0,
+                right: 0.0,
+                child: Container(
+                  width: 16.0,
+                  height: 16.0,
+                  child: Align(
+                    child: Text(
+                      NOTIFICATION_COUNT.toString(),
+                      style: TextStyle(color: Colors.white, fontSize: 11, fontWeight: FontWeight.bold),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.blue[200], borderRadius: BorderRadius.all(const Radius.circular(8.0))),
                 ),
-                alignment: Alignment.center,
-              ),
-              decoration:
-                  BoxDecoration(color: Colors.blue[200], borderRadius: BorderRadius.all(const Radius.circular(8.0))),
-            ),
-          )
-        ]),
+              )
+            ],
+          ),
+        ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: FlatButton(
